ef6b219686f8b98f7e04234f3b04db465b5d4d64
shamoon <4887959+shamoon@users.noreply.github.com>
2023-03-03 19:22:11 -0800

    fix doc detail discard perm changes

--- src-ui/src/app/components/document-detail/document-detail.component.ts
+++ src-ui/src/app/components/document-detail/document-detail.component.ts
@@ -410,6 +410,10 @@ export class DocumentDetailComponent
       .subscribe({
         next: (doc) => {
           Object.assign(this.document, doc)
+          doc['permissions_form'] = {
+            owner: doc.owner,
+            set_permissions: doc.permissions,
+          }
           this.title = doc.title
           this.documentForm.patchValue(doc)
           this.openDocumentService.setDirty(doc, false)
